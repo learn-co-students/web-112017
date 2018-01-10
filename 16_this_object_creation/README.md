49283bb7b2fdbe1e087e47ba59f000c1ea288460
Tim Campbell <timothylevicampbell@gmail.com>
2018-01-10 11:43:16 -0500

    Add notes from lecture

--- 16_this_object_creation/README.md
+++ 16_this_object_creation/README.md
@@ -1,15 +1,19 @@
 # `this` and Object Creation
 * Object literals
-* `this` (execution context: how the function was called)
+* `"use strict";` directive
+  * Strict mode in JavaScript
+  * Insert at top of file or top of function body
+  * More errors raised by JavaScript engine
+  * Less bugs in the long run
+* `this` (execution context: **how** the function was called)
   * global context
   * function context
   * `eval` context (never use!)
 * Calling functions
   * Baseless
-  * Method
-  * `call` and `apply`
   * Bound functions `bind`
-  * Constructor function
+  * `call` and `apply`
+  * Method
 * Object creation
   * Object literals
   * Object literals with methods
@@ -20,3 +24,8 @@
   * Private members with closures
   * Associations
 * Arrow functions
+
+---
+
+## Execution context cheat sheet
+![image](https://camo.githubusercontent.com/841f65aa56d86d2590adb9ef0ab7140961affd87/68747470733a2f2f692e737461636b2e696d6775722e636f6d2f6e50536b582e706e67)
--- 16_this_object_creation/this.js
+++ 16_this_object_creation/this.js
@@ -1,40 +1,9 @@
-// SCOPE DEFINES WHERE
-// EXECUTION CONTEXT DEFINES HOW
-
+// VARIABLE SCOPE defines WHERE a variable is defined
+// EXECUTION CONTEXT defines HOW a function is called
 function showMap(userName) {
-  console.log(`Hello, ${userName}`);
+  console.log(`Hello, ${userName}`)
   console.log(this.name, this.coordinates)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 const googleMaps = {
   name: 'Google Maps',
@@ -46,15 +15,30 @@ const appleMaps = {
   coordinates: [2,3]
 }
 
-// * `call` and `apply`
-// showMap.call(googleMaps, 'tim')
-// showMap.apply(appleMaps, ['tim'])
 
-// * Bound functions `bind`
+// BASELESS function call - executed with GLOBAL CONTEXT
+showMap('alex')
+
+
+// BOUND functions and function calls – `bind` defines context
 const showGoogleMaps = showMap.bind(googleMaps)
 const showAppleMaps = showMap.bind(appleMaps)
 
-// Arrow functions
+showGoogleMaps('fred')
+showAppleMaps('nohemi')
+
+
+// IMMEDIATELY INVOKED BOUND functions – `call` and `apply` define context
+showMap.call(googleMaps, 'matthias')
+showMap.apply(appleMaps, ['joné'])
+
+
+// METHOD defined on an object – object is context
+googleMaps.showMap = showMap
+googleMaps.showMap('steven')
+
+
+// ARROW FUNCTIONS - functions bound to the value of `this` in scope where defined
 const arrowWithMultipleLines = () => {
   console.log(this)
   return 1 + 3
